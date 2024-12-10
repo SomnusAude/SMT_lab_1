3bdc6a931e9fdd108a9454038fb37aebe7cf98db
Lirangx <68253639+Alexander-ha@users.noreply.github.com>
2024-12-10 12:27:03 +0300

    Update gen_matrix_musakimova_data.h

--- gen_matrix_musakimova_data.h
+++ gen_matrix_musakimova_data.h
@@ -50,4 +50,17 @@ double compute_error(double (*analytical)(int, double), gsl_vector* solution, si
 
     return sqrt(error);
 }
+
+
+double compute_inf_error(double (*analytical)(int, double), gsl_vector* solution, size_t n){
+    double error;
+    double h = (1.0 / (n - 1.0));
+    double max_err = 0;
+    for (int i = 0; i < n; i++){
+      if (abs(gsl_vector_get(solution, i) - analytical(i, h))) > max_err{
+            max_err = abs((gsl_vector_get(solution, i) - analytical(i, h)));
+         }
+    }
+    return max_err;
+}
 #endif //GEN_MATRIX_MUSTAKIMOVA_DATA_H
